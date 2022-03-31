db51dd0ba2b973fedea3af1d9841fd6a79f83f92
Hannah Dalwai <hannahdalwai4@gmail.com>
2022-03-31 07:38:10 +0200

    first commit

---
+++ modal-project/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <h1> {{ title }} </h1>
+  <p>Welcome...</p>
+<div v-if="showModal">  <Modal  @close="toggleModal"/></div>
+<button @click="toggleModal">Open Modal</button>
+ <!-- <div class="container"> 
+      <button @click="back"><i class='bx bx-left-arrow-alt bx-fade-left-hover' ></i></button>
+      <button @click="redirect"><i class='bx bxs-home' ></i></button>
+      <button @click="forward"><i class='bx bx-right-arrow-alt bx-fade-right-hover'></i></button> 
+     </div> -->
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: { Modal },
+data(){
+  return{
+    title:'Enjoying the vue',
+    showModal:false,
+    showModalTwo:false,
+  }
+},
+methods: {
+  toggleModal(){
+    this.showModalTwo = !this.showModalTwo
+  },
+  toggleModal(){
+    this.showModalTwo = !this.showModalTwo
+  }
+},
+redirect(){
+       this.$router.push({name:'Home'})
+    },
+    back(){
+      this.$router.go(-1)
+    },
+    forward(){
+       this.$router.go(1)
+    }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+ 
+}
+       h1{
+            border-bottom: 1px solid rgb(73, 53, 37);
+            display:inline-block;
+            padding-bottom: 10px;
+            color: crimson;
+            width: 400px;
+            padding: 20px;
+            margin: 100px auto;
+          background: white;
+          }
+
+</style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="backdrop" @click="closeModal">
+        <div class="modal">
+            <h1>Title of said modal</h1>
+            <p>Modal content</p>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    methods: {
+        closeModal(){
+            this.$emit('close')
+        }
+    },
+}
+</script>
+
+
+<style scoped>
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+        }
+        .backdrop{
+            top: 0;
+            position: fixed;
+            background: rgb(0, 0, 0,0.5);
+            width: 100%;
+            height: 100%;
+        }
+         h1{
+            color: blueviolet;
+            border: none;
+                        border-bottom: 1px solid rgb(191, 89, 250);
+
+        }
+</style>
